a5d790657009d64a8b56cf9e3dd022050136844f
Sergio Flores <sergmauricio@hotmail.com>
2016-06-21 11:23:37 -0600

    Added KO, view models and data models

---
+++ DeveloperInterviewProject/DeveloperInterviewProject/Scripts/non-nuget/dataServices/sampleService.js
@@ -0,0 +1,23 @@
+﻿var packSize = packSize || {};
+
+packSize.sampleDataService = new function (vva) {
+
+    sendCoursesProcess = function (inputObject, callback) {
+        $.ajax({
+            type: "POST",
+            url: "../API/SampleAPI/ProcessOrder",
+            contentType: "application/json",
+            data: JSON.stringify(inputObject),
+            async: true,
+            cache: false,
+            success: function (data) {
+                callback(data);
+            }
+        });
+    };
+
+    return {
+        sendCoursesProcess: sendCoursesProcess
+    };
+
+}();
